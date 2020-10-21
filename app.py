b688b9ccb6d423eb303d5077a9d6da36d0189eeb
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2020-10-21 18:56:15 -0300

    Casas

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from textblob import TextBlob
+import pandas as pd
+from modelos.PrecoCasas import modelo
 
 app = Flask('meu_APP')
-
+model = modelo().linear_casa()
 
 @app.route('/')
 def home():
@@ -15,6 +17,11 @@ def sentimento(frase):
     tb_en = tb.translate(to='en')
     polaridade = tb_en.sentiment.polarity
     return f"polaridade: {polaridade}"
+
+@app.route('/prevercasa/<int:quartos>')
+def cotacao(quartos):
+    preco = model.predict([[quartos]])
+    return f"preco: {preco}"
 
 
 app.run(debug=True)
---
+++ modelos/PrecoCasas.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+class modelo:
+    def __init__(self):
+        pass
+
+    def linear_casa(self):
+        df = pd.read_csv(r'modelos\datas\train.csv')
+        df_personalisado = df[['YearBuilt', 'BedroomAbvGr', 'FullBath', 'GarageArea', 'SalePrice']]
+        df_personalisado.columns = ['Ano', 'QtQuartos', 'QTbanheiros', 'GaragemArea', 'precoVenda']
+        colunas = ['QtQuartos', 'precoVenda']
+        df_filtro = df_personalisado[colunas]
+        x = df_filtro.drop('precoVenda', axis=1)
+        y = df_filtro['precoVenda']
+        X_train, Xtest, Y_train, Y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+        modelo = LinearRegression()
+        modelo.fit(X_train, Y_train)
+        return modelo
+
+
+def retornar_df_filtro(self, filtros, df):
+    colunas = filtros
+    df_filtro = df[colunas]
+    return df_filtro
