b2ad72bee4f867de34eb5005e2b551fbf6592d07
rodrigo.cabral <rodrigo.cabral@radixeng.com.br>
2020-10-25 15:09:45 -0300

    Melhorei o codigo

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
 from textblob import TextBlob
 import pandas as pd
 from modelos.PrecoCasas import modelo
 
 app = Flask('meu_APP')
-model = modelo().linear_casa()
+
 
 @app.route('/')
 def home():
@@ -18,10 +19,15 @@ def sentimento(frase):
     polaridade = tb_en.sentiment.polarity
     return f"polaridade: {polaridade}"
 
-@app.route('/prevercasa/<int:quartos>')
-def cotacao(quartos):
-    preco = model.predict([[quartos]])
-    return f"preco: {preco}"
 
+@app.route('/prevercasa/', methods=['POST'])
+def cotacao():
+    coluna = ['Ano', 'QtQuartos', 'QTbanheiros', 'GaragemArea']
+    model = modelo(coluna).linear_casa()
+    dados = request.get_json()
+    coluna = ['Ano', 'QtQuartos', 'QTbanheiros', 'GaragemArea']
+    dados_json = [dados[col] for col in coluna]
+    preco = model.predict([dados_json])
+    return jsonify(preco=preco[0])
 
 app.run(debug=True)
--- modelos/PrecoCasas.py
+++ modelos/PrecoCasas.py
@@ -4,15 +4,16 @@ from sklearn.linear_model import LinearRegression
 
 
 class modelo:
-    def __init__(self):
+    def __init__(self,colunas):
+        self.colunas = colunas
+        self.colunas.append('precoVenda')
         pass
 
     def linear_casa(self):
         df = pd.read_csv(r'modelos\datas\train.csv')
         df_personalisado = df[['YearBuilt', 'BedroomAbvGr', 'FullBath', 'GarageArea', 'SalePrice']]
         df_personalisado.columns = ['Ano', 'QtQuartos', 'QTbanheiros', 'GaragemArea', 'precoVenda']
-        colunas = ['QtQuartos', 'precoVenda']
-        df_filtro = df_personalisado[colunas]
+        df_filtro = df_personalisado[self.colunas]
         x = df_filtro.drop('precoVenda', axis=1)
         y = df_filtro['precoVenda']
         X_train, Xtest, Y_train, Y_test = train_test_split(x, y, test_size=0.3, random_state=42)
